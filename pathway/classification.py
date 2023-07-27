d6047de04983f8a23870fcb2cfcff5b303103b88
dlwnsrb@1234 <dlwnsrb@kaist.ac.kr>
2023-07-27 17:06:00 +0900

    change classification file
    Former-commit-id: 320c58c88030a77a43afc5a0c0ebc94e4dccec57

--- pathway/classification.py
+++ pathway/classification.py
@@ -23,28 +23,6 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import Subset
 import shutil
 
-model_name = 'efficientnet-b0'
-model = EfficientNet.from_pretrained(model_name,num_classes=2)
-model.load_state_dict(torch.load('best_model.pt'))
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-model = model.to(device)
-
-dataset = datasets.ImageFolder('test',
-                               transforms.Compose([
-                                    transforms.Resize((224, 224)),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                                ]))
-batch_size  = 4
-random_seed = 42
-random.seed(random_seed)
-torch.manual_seed(random_seed)
-dataloaders = torch.utils.data.DataLoader(dataset,
-                                              batch_size=batch_size, shuffle=False,
-                                              num_workers=4)
-
-allFiles, _ = map(list, zip(*dataloaders.dataset.samples))
-
 def evaluate_model(model, dataloader,device):
     model.eval()
     total_result=dict()
@@ -57,11 +35,37 @@ def evaluate_model(model, dataloader,device):
             total_result[image_name]= preds[j]
     return total_result
 
-result= evaluate_model(model,dataloaders,device)
 
-for name,label in result.items():
-    revise_name= name.split('/')[-1]
-    if label==0:
-        shutil.copyfile(name, 'final_output/non_pathway/'+revise_name)
-    else:
-        shutil.copyfile(name, 'final_output/pathway/'+revise_name)
+def classification_image(args)
+    model_name = 'efficientnet-b0'
+    model = EfficientNet.from_pretrained(model_name,num_classes=2)
+    model.load_state_dict(torch.load('model/best_model.pt'))
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    dataset = datasets.ImageFolder(args.output_dir,
+                               transforms.Compose([
+                                    transforms.Resize((224, 224)),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                                ]))
+    batch_size  = 4
+    dataloaders = torch.utils.data.DataLoader(dataset,
+                                              batch_size=batch_size, shuffle=False,
+                                              num_workers=4)
+    allFiles, _ = map(list, zip(*dataloaders.dataset.samples))
+    model.eval()
+    total_result=dict()
+    for i, (inputs, _) in enumerate(dataloader):
+        outputs=model(inputs.to(device))
+        _, preds = torch.max(outputs, 1)
+        preds= list(preds.detach().cpu().numpy())
+        for j in range(inputs.size()[0]):
+            image_name= allFiles[i*batch_size +j]
+            total_result[image_name]= preds[j]
+    for name,label in total_result.items():
+        revise_name= name.split('/')[-1]
+        if label==0:
+            shutil.copyfile(name, 'final_output/non_pathway/'+revise_name)
+        else:
+            shutil.copyfile(name, 'final_output/pathway/'+revise_name)
+    return
